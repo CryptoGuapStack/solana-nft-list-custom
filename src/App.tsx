079155f0d5f2ee833deac6fe6e52becee69910c8
Kwasi Osae-Kwapong <CryptoGuapStack@gmail.com>
2022-08-14 19:35:38 -0400

    Combined git-portal code with solana-nft-list

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,73 @@ import React, { useCallback, useEffect, useState } from "react";
 import { WalletDialogButton } from "@solana/wallet-adapter-material-ui";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { useWalletNfts } from "@nfteyez/sol-rayz-react";
+import twitterLogo from './assets/twitter-logo.svg';
+
+
+// Default styles that can be overridden by your app
+require('./App.css');
+//require('@solana/wallet-adapter-react-ui/styles.css');
+
+// Constants
+const TWITTER_HANDLE = '_buildspace';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TEST_GIF = [
+  'https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp',
+	'https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g',
+	'https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g',
+	'https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp'
+]
+
+
+
 
 export default function App() {
+
+
+//from gif-portal
+
+// This will fetch the users' public key (wallet address) from any wallet we support
+
+
+
+const renderNotConnectedContainer = () => (
+  <div>
+    <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+
+    <div className="button-container">
+
+    <WalletDialogButton />
+
+    </div>    
+  </div>
+);
+
+
+
+const renderConnectedContainer = () => (
+  <div className="connected-container">
+    <div className="nft-grid">
+    {(nfts || []).map((nft) => (
+        <div key={nft.mint}>
+          <img src={metadata?.[nft.mint]?.image} alt="nft" width="200" />
+          <br />
+          {nft.data.name}
+        </div>
+      ))}
+    </div>
+  </div>
+)
+
+
+
+
+
+
+
+
+// end from gif-portal
+
+
   const wallet = useWallet();
   const { connection } = useConnection();
   const { nfts } = useWalletNfts({
@@ -29,16 +94,39 @@ export default function App() {
 
   return (
     <>
-      <h1>Solana NFT List</h1>
-      <WalletDialogButton />
-      <hr />
-      {(nfts || []).map((nft) => (
-        <div key={nft.mint}>
-          <img src={metadata?.[nft.mint]?.image} alt="nft" width="200" />
-          <br />
-          {nft.data.name}
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header">🖼 NFT Portal</p>
+          <p className="sub-text">
+            View your NFT collection in the metaverse ✨
+          </p>
+          <main>
+
+          
+            {/* We only render the connect button if public key doesn't exist */}
+            {wallet?.publicKey ? 'Connected!' : renderNotConnectedContainer()}
+            {wallet?.publicKey && renderConnectedContainer()}
+          </main>
         </div>
-      ))}
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built on @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+
+
+
+ 
+      
+      <hr />
+
     </>
   );
 }
--- src/contexts/SolanaProvider.tsx
+++ src/contexts/SolanaProvider.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useMemo } from "react";
+import { useEffect, useState, useMemo } from "react";
 import {
   getPhantomWallet,
   getSolflareWallet,
@@ -7,17 +7,37 @@ import {
 import {
   ConnectionProvider,
   WalletProvider,
+  useWallet
 } from "@solana/wallet-adapter-react";
 import { WalletDialogProvider } from "@solana/wallet-adapter-material-ui";
+import { clusterApiUrl, PublicKey } from "@solana/web3.js";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+
 
 // const ENDPOINT = "https://api.devnet.solana.com";
-const ENDPOINT = "https://api.mainnet-beta.solana.com";
+
+
+
 
 export default function App({ children }) {
+
+      //state
+      const [walletAddress, setWalletAddress] = useState(null);
+
+      // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+      const network = WalletAdapterNetwork.Mainnet;
+
+   // This will fetch the users' public key (wallet address) from any wallet we support
+   const { publicKey } = useWallet();
+
+   // You can also provide a custom RPC endpoint.
+   const endpoint = useMemo(() => clusterApiUrl(network), [network])
+
+
   const wallets = useMemo(() => [getPhantomWallet(), getSolflareWallet()], []);
 
   return (
-    <ConnectionProvider endpoint={ENDPOINT}>
+    <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets} autoConnect>
         <WalletDialogProvider>{children}</WalletDialogProvider>
       </WalletProvider>
